181311733bdabba543b73a2ff9b217b7ac49b2a3
pfcm <pfcmathews@gmail.com>
2018-07-30 11:29:00 +1200

    streaming works, just want to print counts to stderr

--- app/FilterMain.hs
+++ app/FilterMain.hs
@@ -16,7 +16,6 @@ import qualified Data.Conduit.List as CL
 import Data.Maybe
 import Data.Semigroup ((<>))
 import qualified Data.Text as T
-import Debug.Trace
 import Options.Applicative
 import Web.Twitter.Conduit
 import Web.Twitter.Types
@@ -25,6 +24,7 @@ data FilterOptions = FilterOptions
   { auth :: Maybe OAuth
   , rcfile :: String
   , keywords :: [T.Text]
+  , numTweets :: Int
   } deriving (Show)
 
 keywordsParser :: Parser [T.Text]
@@ -56,8 +56,18 @@ authParser =
   strOption (long "consumer_key" <> help "consumer key for twitter api") <*>
   strOption (long "consumer_secret" <> help "consumer secret for twitter api")
 
+numTweetsParser :: Parser Int
+numTweetsParser =
+  option
+    auto
+    (long "num" <> short 'n' <> metavar "INT" <>
+     help "how many tweets to grab before exiting" <>
+     value 100)
+
 optionParser :: Parser FilterOptions
-optionParser = FilterOptions <$> authParser <*> rcPathParser <*> keywordsParser
+optionParser =
+  FilterOptions <$> authParser <*> rcPathParser <*> keywordsParser <*>
+  numTweetsParser
 
 cmdOpts :: ParserInfo FilterOptions
 cmdOpts = info (helper <*> optionParser) (descriptionHeader <> fullDesc)
@@ -68,25 +78,38 @@ makeFilterRequest = statusesFilter . pure . Track
 truncated :: Value -> Maybe Bool
 truncated v = v ^? key "truncated" . _Bool
 
-extendedText :: Value -> Maybe B.ByteString
-extendedText v =
-  case trace ("truncated: " ++ (show . truncated $v)) fullText of
-    "\"\"" -> Nothing
-    x -> Just x
-  where
-    fullText = encode $ v ^. key "extended_tweet" . key "full_text" . _String
+statusLanguage :: Value -> T.Text
+statusLanguage v = v ^. key "lang" . _String
+
+extendedText :: Value -> B.ByteString
+extendedText v = encode $ v ^. key "extended_tweet" . key "full_text" . _String
 
 normalText :: Value -> B.ByteString
 normalText v = encode $ v ^. key "text" . _String
 
-fullStatusText :: Value -> Maybe B.ByteString
-fullStatusText v = extendedText v <|> Just (normalText v)
+fullStatusText :: Value -> B.ByteString
+fullStatusText v =
+  case extendedText v of
+    "\"\"" -> normalText v
+    x -> x
 
 isRetweet :: Value -> Bool
 isRetweet v = isJust rt
   where
     rt = v ^? key "retweeted_status"
 
+isLang :: T.Text -> Value -> Bool
+isLang l v = statusLanguage v == l
+
+zipC ::
+     Monad m => ConduitT a b m r -> ConduitT () c m r -> ConduitT a (b, c) m r
+zipC a b = do
+  aVal <- await a
+  bVal <- await b
+  yield (aVal, bVal)
+
+-- countAndPrint :: Monad m => ConduitT a (IO a) m ()
+-- countAndPrint c = let
 main :: IO ()
 main = do
   opts <- execParser cmdOpts
@@ -97,7 +120,7 @@ main = do
   runResourceT $ do
     response <-
       stream' twinfo mgr req :: (ResourceT IO) (ConduitT () Value (ResourceT IO) ())
-    runConduit $ response .| filterC isRetweet .| takeC 10 .|
+    runConduit $ response .| filterC (not . isRetweet) .| filterC (isLang "en") .|
+      takeC (numTweets opts) .|
       mapC fullStatusText .|
-      CL.catMaybes .|
       mapM_C (lift . B.putStrLn)
